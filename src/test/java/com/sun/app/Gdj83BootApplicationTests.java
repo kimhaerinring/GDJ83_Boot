cb4f2c27c17d971a500f724c68d1b0cf1821b117
kimhaerinring <rlagofls0824@naver.com>
2024-09-03 10:09:32 +0900

    TEST

---
+++ src/test/java/com/sun/app/Gdj83BootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sun.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gdj83BootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
